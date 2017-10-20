3fe9fd6d719112ce6f80107d21fddbe868c7afb5
toelapiut <toelapiut7@gmail.com>
2017-10-20 12:12:37 +0300

    stripping the source url

--- app/request.py
+++ app/request.py
@@ -10,33 +10,50 @@ api_key=app.config['NEWS_API_KEY']
 article_url=app.config['NEWS_ARTICLE_API_BASE_URL']
 source_url=app.config['NEWS_SOURCE_API_BASE_URL']
 
-#getting categories of News
 
 
+#Stripping the source url to get information we need 
 
-def get_news(sortby):
-
-	get_sources_url=article_url.format(sortby,)
-
-	with urllib.request.urlopen(get_sources_url) as url:
-		get_source_data=url.read()
+def get_url(sources):
+	with urllib.request.urlopen(source_url) as sourceUrl:
+		get_source_data=sourceUrl.read()
 		get_source_response=json.loads(get_source_data)
+
 
 		source_results=None
 
-		if get_source_response['results']:
-			source_results_list=get_source_response['results']
-			source_results=process_results(source_results_list)
+		if get_source_response['result']:
+			source_result_list=get_source_response['result']
+			source_results=process_source_result(source_result_list)
+
+#---------------------------------------------------------
 
 
-	return source_results
+
+#rem to work here
+# def get_news(sortby):
+
+# 	get_articles_url=article_url.format()
+
+# 	with urllib.request.urlopen(get_article_url) as url:
+# 		get_article_data=url.read()
+# 		get_article_response=json.loads(get_article_data)
+
+# 		article_results=None
+
+# 		if get_artcle_response['results']:
+# 			article_results_list=get_article_response['results']
+# 			article_results=process_results(article_results_list)
 
 
-	def process_results(source_list):
+# 	return source_results
 
 
-		source_output=[]
+# 	def process_results(source_list):
 
-		for source_item in source_list:
+
+# 		source_output=[]
+
+# 		for source_item in source_list:
 
 
